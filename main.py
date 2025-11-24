3efc1fa3552ba91c8b626ba2b7c5c479c5218af6
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2025-11-24 15:34:32 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+#1-misol
+matn = " salom dunyo "
+print(f"Matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+#2-misol
+matn = "o'zbekiston   respublikasi"
+print(f"Matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+#3-misol
+matn = "123 456 789"
+print(f"Matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+#4-misol
+matn = "               "
+print(f"Matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+#5-misol
+matn = "      "
+print(f"Matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
